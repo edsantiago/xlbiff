a908fafc9625b18c291fcb24472655735833d1db
santiago <santiago>
1991-08-20 01:02:18 +0000

    checkpoint: got resources, checkfile working

--- xlbiff.c
+++ xlbiff.c
@@ -1,8 +1,17 @@
+/*\
+|* xlbiff  --  X Literate Biff
+|*
+|* This application does mumble
+\*/
+
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <signal.h>
 #include <setjmp.h>
+#include <pwd.h>
+#include <unistd.h>
 
 #include <X11/Intrinsic.h>
 #include <X11/StringDefs.h>
@@ -18,12 +27,20 @@
 #endif
 
 
-/*
-** globals
-*/
+#if	1
+#define	MAILPATH	"/usr/spool/mail/%s"
+#else
+#define	MAILPATH	"/udir/%s/.mailbox"
+#endif
+
+
+/*****************************************************************************\
+** globals								     *|
+\*****************************************************************************/
 Widget	topLevel,goodbye;
 jmp_buf	myjumpbuf;
 int	visible;
+char	default_file[80];
 
 typedef struct {
     Boolean	debug;
@@ -51,7 +68,10 @@ static XrmOptionDescRec	optionDescList[] = {
     { "-file",	"*file",	XrmoptionSepArg,	(caddr_t) NULL}
 };
 
-
+static char *fallback_resources[] = {
+    "XLbiff*font:	-*-clean-bold-r-normal--13-130-75-75-c-80-iso8859-1",
+    NULL
+  };
 /*
 ** prototypes
 */
@@ -75,13 +95,30 @@ Quit(Widget w, XtPointer client_data, XtPointer call_data)
 \*/
 main( int argc, char *argv[] )
 {
+    char *username;
     XtAppContext app_context;
 
+    /*
+    ** Get user name, in case no explicit path is given
+    */
+    if ((username= getlogin()) == NULL) {
+	struct passwd *pwd= getpwuid(getuid());
+
+	if (pwd == NULL) {
+	    fprintf(stderr,"%s: cannot get username\n",argv[0]);
+	    exit(1);
+	}
+	username = pwd->pw_name;
+    }
+
+    /*
+    ** Do all the X stuff
+    */
     topLevel = XtVaAppInitialize(&app_context,
 				 "XLbiff",
 				 optionDescList, XtNumber(optionDescList),
 				 &argc, argv,
-				 NULL,
+				 fallback_resources,
 				 NULL);
 
     goodbye = XtVaCreateManagedWidget("goodbye",
@@ -96,16 +133,34 @@ main( int argc, char *argv[] )
 
     XtAddCallback(goodbye,XtNcallback, Quit, goodbye );
 
+    /*
+    ** If no data file was explicitly given, make our best guess
+    */
+    if (lbiff_data.file == NULL) {
+	sprintf(default_file,MAILPATH,username);
+	lbiff_data.file = default_file;
+    }
+
+    /*
+    ** check to see if there's something to do, pop up window if necessary,
+    ** and set up alarm to wake us up again every so often.
+    */
     checksize();
     signal(SIGALRM,handler);
     alarm(10);
 
+    /*
+    ** main program loop  --  mostly just loops forever waiting for events
+    **
+    ** note that we will continually be interrupted by the timeout code
+    */
     setjmp(myjumpbuf);
     if (visible) {
 	while (1) {
 	    XEvent ev;
 
 	    XtAppNextEvent(app_context,&ev);
+	    DEBUG(("got event\n"));
 	    XtDispatchEvent(&ev);
 	}
 /*	XtAppMainLoop(app_context);*/
@@ -192,11 +247,9 @@ doscan()
 }
 
 
-
-
-/*\
-|*  popdown  --  kill window
-\*/
+/*****************************\
+|*  popdown  --  kill window *|
+\*****************************/
 popdown()
 {
     DEBUG(("++popdown()..."));
@@ -206,9 +259,10 @@ popdown()
     visible = 0;
 }
 
-/*\
-|*  popup  --  bring window up
-\*/
+
+/*******************************\
+|*  popup  --  bring window up *|
+\*******************************/
 popup()
 {
     DEBUG(("++popup()\n"));
